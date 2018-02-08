172117c935422082d139faa007f424bd37da7dd1
jw <test@test.com>
2018-02-08 23:18:38 +0900

    스프링 시큐리티 자동 구성 오버라이드 적용

--- src/main/java/com/study/readlinglist/controller/ReadingListController.java
+++ src/main/java/com/study/readlinglist/controller/ReadingListController.java
@@ -9,13 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.study.readlinglist.domain.Book;
+import com.study.readlinglist.domain.Reader;
 import com.study.readlinglist.repository.ReadingListRepository;
 
 @Controller
 @RequestMapping("/")
 public class ReadingListController {
-	private static final String reader = "jw";
-	
 	private ReadingListRepository readingListRepository;
 	
 	@Autowired
@@ -24,17 +23,18 @@ public class ReadingListController {
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String readersBooks(Model model) {
+	public String readersBooks(Reader reader, Model model) { // Reader reader: ReaderHandlerMethodArgumentResolver의 resolveArgument()에 의해 바인딩.
 		List<Book> readingList = readingListRepository.findByReader(reader);
 		if (readingList != null) {
 			model.addAttribute("books", readingList);
+			model.addAttribute("reader", reader);
 		}
 		
 		return "readingList";
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public String addToReadingList(Book book) {
+	public String addToReadingList(Reader reader, Book book) {
 		book.setReader(reader);
 		readingListRepository.save(book);
 		
---
+++ src/main/java/com/study/readlinglist/resolver/ReaderHandlerMethodArgumentResolver.java
@@ -0,0 +1,30 @@
+package com.study.readlinglist.resolver;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import com.study.readlinglist.domain.Reader;
+
+/*
+ * HandlerMethodArgumentResolver 인터페이스는 컨트롤러에서 파라미터를 바인딩해주는 역할을 한다.
+ */
+public class ReaderHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
+
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) { // 바인딩할 클래스 지정.
+		return Reader.class.isAssignableFrom(parameter.getParameterType());
+	}
+
+	@Override
+	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+			NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception { // 바인딩할 클래스 조작.
+		
+		Authentication auth = (Authentication) webRequest.getUserPrincipal();
+		return auth != null && auth.getPrincipal() instanceof Reader ? auth.getPrincipal() : null;
+	}
+
+}
